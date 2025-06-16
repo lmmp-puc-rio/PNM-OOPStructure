04352521c4852bfda0f0b29dce0a5c53a620adab
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2025-06-16 20:01:44 +0000

    [update] cubic network 2d drainage

--- src/examples/cubic_network_dr_2D.py
+++ src/examples/cubic_network_dr_2D.py
@@ -19,12 +19,13 @@ path = os.path.dirname(__file__)
 """First, we create a network and assign phases and properties in a similar way that we used to do for the other examples.""" 
 
 # Creating a Cubic Network
-npores = 10
-np.random.seed(5)
+npores = 25
+np.random.seed(15)
 fps = 10
 trapping = 'trapping'  # Options: 'trapping', 'no_trapping'
 Lc = 1e-6
-pn = op.network.Cubic(shape=[npores, npores, 1], spacing=1e-4)
+spacing = 1e-4
+pn = op.network.Cubic(shape=[npores, npores, 1], spacing=spacing)
 pn.add_model_collection(op.models.collections.geometry.spheres_and_cylinders)
 pn.regenerate_models()
 pn['pore.diameter'] = np.random.rand(pn.Np)*Lc
@@ -43,9 +44,9 @@ for f in files:
 # Pore size distribution
 air['pore.contact_angle'] = 120
 air['pore.surface_tension'] = 0.072
-f = op.models.physics.capillary_pressure.washburn
+washburn = op.models.physics.capillary_pressure.washburn
 air.add_model(propname='throat.entry_pressure',
-              model=f, 
+              model=washburn, 
               surface_tension='throat.surface_tension',
               contact_angle='throat.contact_angle',
               diameter='throat.diameter',)
@@ -60,36 +61,35 @@ fig1.savefig(path+"/results/DR_"+trapping+"/videos/frames_"+str(npores)+"pores/f
 
 fig3,ax3 = plt.subplots()
 dr = op.algorithms.Drainage(network=pn, phase=air)
-np.random.seed(5)
 Inlet = pn.pores('left')
 Outlet = pn.pores('right')
 pn['pore.volume'][Inlet] = 0.0
 dr.set_inlet_BC(pores=Inlet)
-dr.set_outlet_BC(pores=Outlet)
+# dr.set_outlet_BC(pores=Outlet)
 
-dr.run()
-data_ip_no_trapping = dr.pc_curve()
+dr.run(pressures=100)
+data_ip_no_trapping = dr.pc_curve(pressures=100)
 
 ## Apply Trapping
 if trapping == 'trapping':
     dr.set_outlet_BC(pores=Outlet, mode='overwrite')
     dr.apply_trapping()
 
-data_ip = dr.pc_curve(pressures=50)
+data_ip = dr.pc_curve(pressures=100)
 
 image_files = []
 
 op.visualization.plot_coordinates(pn, size_by=pn['pore.diameter'], markersize=50, c='b', ax=ax3);
-op.visualization.plot_connections(pn, pn.throats() ,size_by=pn['throat.diameter'], linewidth=3, c='b' ,ax=ax3);
+op.visualization.plot_connections(pn, pn.throats() ,size_by=pn['throat.diameter'], linewidth=3, c='b' ,ax=ax3)
 op.visualization.plot_coordinates(pn, pn.pores('left'), size_by=pn['pore.diameter'], c='r', ax=ax3)
 fig3.savefig(path+"/results/DR_"+trapping+"/videos/frames_"+str(npores)+"pores/frame0.png")
 
 
-for sequence in range(1, len(dr['throat.invasion_sequence']), 1):
+for sequence in np.unique(dr['throat.invasion_sequence'][dr['throat.invasion_sequence']!= np.inf]):
     inv_throat_pattern = dr['throat.invasion_sequence'] <= sequence
     inv_pore_pattern = dr['pore.invasion_sequence'] <= sequence
     op.visualization.plot_coordinates(pn, inv_pore_pattern, size_by=pn['pore.diameter'], markersize=50, c='r', ax=ax3)
-    op.visualization.plot_connections(pn, inv_throat_pattern,size_by=pn['throat.diameter'], linewidth=3, c='r' ,ax=ax3);
+    op.visualization.plot_connections(pn, inv_throat_pattern,size_by=pn['throat.diameter'], linewidth=3, c='r' ,ax=ax3)
     # ax3.legend(['water_pores', 'water_throats', 'gas_pores', 'gas_throats'], loc='best')
     fig3.savefig(path+"/results/DR_"+trapping+"/videos/frames_"+str(npores)+"pores/frame"+str(sequence)+".png")
     image_files.append(path+"/results/DR_"+trapping+"/videos/frames_"+str(npores)+"pores/frame"+str(sequence)+".png")
@@ -117,4 +117,3 @@ ax5.set_title('Cubic Network with Invasion Percolation')
 # ax5.set_xlim(1e5, 1e8)
 ax5.set_ylim(0, 105)
 fig5.savefig(path+"/results/DR_"+trapping+"/CapillaryPressureXSaturation.png")
-
