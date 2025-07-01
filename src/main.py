477373e52ffe115d6a624583e0d002a7957d84f9
Stelio Lopes <steliohlopes@gmail.com>
2025-07-01 19:31:33 +0000

    [add] init phase using main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from utils.config_parser import ConfigParser
 from core.network import Network
+from core.phases import Phases
 
 json_file = 'data/base.json'
 
 cfg = ConfigParser.from_file(json_file)
 
-pn = Network(cfg)
+pn = Network(config = cfg)
+phases = Phases(network = pn, config = cfg)
