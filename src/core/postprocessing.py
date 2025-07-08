da443850350dfca1c70b057d2dfae35e8521a6ff
Stelio Lopes <steliohlopes@gmail.com>
2025-07-08 12:05:53 +0000

    [fix] fix some plot configurations, adding zorder to make pores draw on top of throats

--- src/core/postprocessing.py
+++ src/core/postprocessing.py
@@ -20,27 +20,22 @@ class PostProcessing:
     def plot_network(self, lwidth=3, msize=100, azim=-60, elev=15):
         pn = self.algorithm.network.network
         phases = self.algorithm.phases.phases
-        algorithm = self.algorithm
+        dim =  self.algorithm.network.dim
         linewidth = pn['throat.diameter'] / pn['throat.diameter'].max() * lwidth + (lwidth/3)
         markersize = pn['pore.diameter'] / pn['pore.diameter'].max() * msize + msize
-        Np_col = len(np.unique(pn.coords.T[0]))
-        Np_row = len(np.unique(pn.coords.T[1]))
-        pn_dim = '2D'
-        if len(np.unique(pn['pore.coords'].T[2])) > 1:
-            pn_dim = '3D'
         fig0 = plt.figure()
-        ax0 = fig0.add_subplot(111, projection = '3d') if pn_dim == '3D' else fig0.add_subplot(111)
-        fig0.set_size_inches(Np_col,Np_row)
+        ax0 = fig0.add_subplot(111, projection = '3d') if dim == '3D' else fig0.add_subplot(111)
+        fig0.set_size_inches(10,10)
         ax0.set_aspect('auto')
         ax0.ticklabel_format(style='sci', axis='both', scilimits=(0, 0))
         ax0.grid(False)
         ax0.set_title(f'Pore Network',fontsize=16)
-        if pn_dim == '3D':
+        if dim == '3D':
             ax0.view_init(elev=elev, azim=azim)
-        first_phase = algorithm.algorithm[0].settings.phase
+        first_phase = self.algorithm.algorithm[0].settings.phase
         phase_ic_color = next(p["color"] for p in phases if p["name"] != first_phase)
-        op.visualization.plot_coordinates(pn, markersize=markersize, c=phase_ic_color, alpha=0.8, ax=ax0)
-        op.visualization.plot_connections(pn, linewidth=linewidth, c=phase_ic_color, alpha=0.8, ax=ax0)
+        op.visualization.plot_coordinates(pn, markersize=markersize, c=phase_ic_color,zorder=2 ,alpha=0.8, ax=ax0)
+        op.visualization.plot_connections(pn, linewidth=linewidth, c=phase_ic_color,zorder=1 ,alpha=0.8, ax=ax0)
         fig0.savefig(os.path.join(self.graph_path, f'Network_{self.algorithm.network.project_name}.png'))
         plt.close(fig0)
         return
@@ -80,49 +75,50 @@ class PostProcessing:
                       throats_ic, pores_ic):
         plt.sca(ax)
         self._clear_ax(ax)
-        for x, y, p in zip(x_throat, y_throat, entry_pressure):
-            ax.text(x, y, f'{p:.2f}', fontsize=6,
-                    ha='center', va='center', color='black', zorder=3)
+        # Plot the throat invasion pressure as text on the plot
+        # for x, y, p in zip(x_throat, y_throat, entry_pressure):
+        #     ax.text(x, y, f'{p:.2f}', fontsize=6,
+        #             ha='center', va='center', color='black', zorder=3)
         if throats_ic.size:
             op.visualization.plot_connections(
-                pn.network, throats_ic, alpha=0.5,
-                linewidth=linewidth[throats_ic], c=inv_color, ax=ax)
+                pn, throats_ic, alpha=0.5,
+                linewidth=linewidth[throats_ic], c=inv_color, zorder=1, ax=ax)
         if pores_ic.size:
             op.visualization.plot_coordinates(
-                pn.network, pores_ic, alpha=0.5,
-                markersize=markersize[pores_ic], c=inv_color, ax=ax)
-        throats_not_ic = np.setdiff1d(pn.network.Ts, throats_ic)
-        pores_not_ic   = np.setdiff1d(pn.network.Ps, pores_ic)
+                pn, pores_ic, alpha=0.5,
+                markersize=markersize[pores_ic], c=inv_color,zorder=2, ax=ax)
+        throats_not_ic = np.setdiff1d(pn.Ts, throats_ic)
+        pores_not_ic   = np.setdiff1d(pn.Ps, pores_ic)
         if throats_not_ic.size:
             op.visualization.plot_connections(
-                pn.network, throats_not_ic, alpha=0.8,
-                linewidth=linewidth[throats_not_ic], c=not_inv_color, ax=ax)
+                pn, throats_not_ic, alpha=0.8,
+                linewidth=linewidth[throats_not_ic], c=not_inv_color, zorder=1, ax=ax)
         if pores_not_ic.size:
             op.visualization.plot_coordinates(
-                pn.network, pores_not_ic, alpha=0.8,
-                markersize=markersize[pores_not_ic], c=not_inv_color, ax=ax)
+                pn, pores_not_ic, alpha=0.8,
+                markersize=markersize[pores_not_ic], c=not_inv_color,zorder=2, ax=ax)
         mask_throat = alg['throat.invasion_sequence'] <= sequence
         mask_pore   = alg['pore.invasion_sequence']   <= sequence
-        new_throats = pn.network.Ts[mask_throat]
-        new_pores   = pn.network.Ps[mask_pore]
+        new_throats = pn.Ts[mask_throat]
+        new_pores   = pn.Ps[mask_pore]
         still_not_t = np.setdiff1d(throats_not_ic, new_throats)
         still_not_p = np.setdiff1d(pores_not_ic,   new_pores)
         if new_throats.size:
             op.visualization.plot_connections(
-                pn.network, new_throats, alpha=0.8,
-                linewidth=linewidth[new_throats], c=inv_color, ax=ax)
+                pn, new_throats, alpha=0.8,
+                linewidth=linewidth[new_throats], c=inv_color, zorder=1, ax=ax)
         if still_not_t.size:
             op.visualization.plot_connections(
-                pn.network, still_not_t, alpha=0.8,
-                linewidth=linewidth[still_not_t], c=not_inv_color, ax=ax)
+                pn, still_not_t, alpha=0.8,
+                linewidth=linewidth[still_not_t], c=not_inv_color, zorder=1, ax=ax)
         if new_pores.size:
             op.visualization.plot_coordinates(
-                pn.network, new_pores, alpha=0.8,
-                markersize=markersize[new_pores], c=inv_color, ax=ax)
+                pn, new_pores, alpha=0.8,
+                markersize=markersize[new_pores], c=inv_color,zorder=2, ax=ax)
         if still_not_p.size:
             op.visualization.plot_coordinates(
-                pn.network, still_not_p, alpha=0.8,
-                markersize=markersize[still_not_p], c=not_inv_color, ax=ax)
+                pn, still_not_p, alpha=0.8,
+                markersize=markersize[still_not_p], c=not_inv_color,zorder=2, ax=ax)
         p_kpa = alg['throat.invasion_pressure'][
                   alg['throat.invasion_sequence'] == sequence].max() / 1000
         ax.set_title(f'Pressure = {p_kpa:.2f} kPa', fontsize=10)
