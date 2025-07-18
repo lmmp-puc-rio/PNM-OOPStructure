da2ee3a5263f491d83399c1e1f407951f04255d0
Stelio Lopes <steliohlopes@gmail.com>
2025-07-18 19:18:31 +0000

    [add] Stokes Algorithm test file

---
+++ src/stokes.py
@@ -0,0 +1,20 @@
+from utils.config_parser import ConfigParser
+from core.network import Network
+from core.phases import Phases
+from core.algorithm import Algorithm
+from core.postprocessing import PostProcessing
+import os
+
+base_path = os.path.dirname(__file__)
+json_file = 'data/non_newtonianStokes.json'
+
+cfg = ConfigParser.from_file(json_file)
+
+pn = Network(config = cfg)
+phases = Phases(network = pn, config = cfg)
+algorithm = Algorithm(network = pn, phases = phases,config = cfg)
+algorithm.run()
+
+post = PostProcessing(algorithm=algorithm, base_path=base_path)
+post.plot_network()
+post.plot_absolute_permeability(alg=algorithm.algorithm[0])
