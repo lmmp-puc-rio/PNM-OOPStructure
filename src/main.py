6f2e943a5b137b28e3c03934ddcee02855647e7f
Stelio Lopes <steliohlopes@gmail.com>
2025-07-29 17:24:39 +0000

    [update] refactor post-processing imports and initialization in main and stokes modules

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from utils.config_parser import ConfigParser
 from core.network import Network
 from core.phases import Phases
 from core.algorithms import AlgorithmManager
-from core.postprocessing import PostProcessing
+from core.postprocessing import PostProcessingManager
 import os
 from utils.figures.media_utils import make_video, save_images_side_by_side
 
@@ -10,8 +10,18 @@ base_path = os.path.dirname(__file__)
 json_file = 'data/base.json'
 
 cfg = ConfigParser.from_file(json_file)
+pn = Network(config=cfg)
+phases = Phases(network=pn, config=cfg)
 
-pn = Network(config = cfg)
-phases = Phases(network = pn, config = cfg)
 manager = AlgorithmManager(pn, phases, cfg)
 results = manager.run_all()
+
+postproc = PostProcessingManager(manager, base_path)
+postproc.plot_network()
+postproc.drainage_processor.plot_capillary_pressure_curve('drainageSimulation')
+postproc.drainage_processor.make_invasion_frames('drainageSimulation')
+postproc.drainage_processor.make_invasion_frames('imbibitionSimulation')
+postproc.drainage_processor.make_clusters_frames('drainageSimulation')
+postproc.drainage_processor.make_clusters_frames('imbibitionSimulation')
+postproc.drainage_processor.plot_relative_permeability('drainageSimulation')
+postproc.drainage_processor.plot_relative_permeability('imbibitionSimulation')
--- src/stokes.py
+++ src/stokes.py
@@ -2,15 +2,20 @@ from utils.config_parser import ConfigParser
 from core.network import Network
 from core.phases import Phases
 from core.algorithms import AlgorithmManager
-from core.postprocessing import PostProcessing
+from core.postprocessing import PostProcessingManager
 import os
 
 base_path = os.path.dirname(__file__)
 json_file = 'data/non_newtonianStokes.json'
 
 cfg = ConfigParser.from_file(json_file)
+pn = Network(config=cfg)
+phases = Phases(network=pn, config=cfg)
 
-pn = Network(config = cfg)
-phases = Phases(network = pn, config = cfg)
 manager = AlgorithmManager(pn, phases, cfg)
 results = manager.run_all()
+
+postproc = PostProcessingManager(manager, base_path)
+postproc.plot_network(lwidth=3, msize=100)
+postproc.stokes_processor.plot_absolute_permeability('stokesSimulation')
+postproc.stokes_processor.plot_pressure_field('stokesSimulation')
